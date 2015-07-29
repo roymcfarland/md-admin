421cb259bde6d61a8c4818c452228e62a6b7967f
Roy McFarland <roy.mcfarland@gmail.com>
2015-07-28 23:28:23 -0600

    add doLogin(  ) to ng-click

--- public/js/controllers/loginCtrl.js
+++ public/js/controllers/loginCtrl.js
@@ -4,13 +4,34 @@ loginCtrl.controller('loginController', ['$scope', '$state', '$http', function($
 
 	$scope.vm = this;
 
-	$scope.doLogin = function() {
+	$scope.doLogin = function(username, password) {
 		
 		// ajax
-		
+		var payload = {
+			username: username,
+			password: password
+		}
+		$http.post('http://snaportationvm.cloudapp.net/auth/phone/login', payload)
+			.success(function(data, status) {
+				
+				// dynamically set username and token in localStorage
+				var username = data.local.username;
+				var token = data.local.token;
+				window.localStorage['username'] = username;
+				window.localStorage['token'] = token;
+				
+				// post-login navigation
+				$state.go('dashboard');
 
-		// navigation
-		$state.go('dashboard');
+			})
+			.error(function(data, status) {
+				
+				// error handling
+				console.log("error status:", status);
+				console.log("error data:", data);
+
+			})
+
 	}
 
 }]);
